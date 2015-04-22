1d2ed8a716eccbea113a0f3afedd97fd00faec4f
Owen Chiaventone <omc8db@mst.edu>
2015-04-22 16:42:02 -0500

    Create project

---
+++ Maxflow.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Maxflow.cpp
+// Author      : Owen Chiaventone
+// Version     :
+// Copyright   : (c) 2015 Owen Chiaventone
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
